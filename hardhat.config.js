0917838c4a137dea08605fbcc55349f7bdfabd96
Slamet Raharjo <himura.slara@gmail.com>
2024-07-24 09:16:13 +0000

    Swisstronik Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0xba4f20bb0e5685bd6db84f6556b7efd271c7c51b51113e673de5a1226f06d40f"], //Your private key starting with "0x" 
+      accounts: ["0xYourPrivateKey"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
